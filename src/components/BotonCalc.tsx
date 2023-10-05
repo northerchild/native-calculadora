12f8b133acc35df02541a2d7e49bc67ec4d0c285
jhonata ospina <osarus-13@hotmail.com>
2023-10-05 14:38:22 -0500

    calculadora finalizada

---
+++ src/components/BotonCalc.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { styles } from '../theme/appTheme';
+
+interface Props {
+    texto: string,
+    color?: string,
+    ancho?: boolean,
+    accion: (numeroTexto:string) => void;
+}
+
+const BotonCalc = ({texto, color="#2d2d2d", ancho = false, accion}:Props) => {
+  return (
+    <TouchableOpacity
+            onPress={ () => accion( texto ) }
+        >
+            <View style={{ 
+                ...styles.boton ,
+                backgroundColor: color,
+                width: ( ancho ) ? 180 : 80
+            }}>
+                <Text style={{ 
+                    ...styles.botonTexto,
+                    color: ( color === '#9B9B9B' ) ? 'black' : 'white'
+                }}>{ texto }</Text>
+            </View>
+        </TouchableOpacity>
+  )
+}
+
+export default BotonCalc
